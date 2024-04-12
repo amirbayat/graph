110e34babeb8ddcea8400bdd6787a47229a9ab97
4amirbayat <4amirbayat@gmail.com>
2024-04-12 13:59:21 +0330

    added function to generate random nodes with edged

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,20 +1,10 @@
-import React, { useRef, useEffect } from "react";
+import { useCanvas } from "../hooks/useCanvas";
+import { TNode } from "../types/node.type";
+import { generateNodes } from "../utils/generateNodes";
 
-type Props = {
-  zoom?: number;
-};
-const Canvas = (props: Props) => {
-  const { zoom = 1 } = props;
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas == null) return;
-    const ctx = canvas.getContext("2d");
-    if (ctx == null) return;
-    canvas.width = window.innerWidth;
-    ctx.fillStyle = "#ddd";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }, []);
+const Canvas = () => {
+  const { canvasRef } = useCanvas();
+  const nodes = generateNodes();
   return <canvas height={600} width={600} ref={canvasRef}></canvas>;
 };
 
