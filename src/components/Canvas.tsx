db7be9e8c9c33bd9c8a9fdccf62fd7148902c1bd
4amirbayat <4amirbayat@gmail.com>
2024-04-12 14:06:33 +0330

    added action button to generate nodes and zoom button

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
 import { useCanvas } from "../hooks/useCanvas";
 import { TNode } from "../types/node.type";
 import { generateNodes } from "../utils/generateNodes";
+import Actions from "./Actions";
 
 const Canvas = () => {
   const { canvasRef } = useCanvas();
-  const nodes = generateNodes();
-  return <canvas height={600} width={600} ref={canvasRef}></canvas>;
+  const [zoom, setZoom] = useState<number>(1);
+  function generate() {
+    const nodes = generateNodes();
+  }
+
+  function zoomIn() {
+    setZoom(Number((zoom + 0.1).toFixed(2)));
+    // renderGraph(nodesRef.current);
+  }
+
+  function zoomOut() {
+    if (zoom > 1) {
+      setZoom(Number((zoom - 0.1).toFixed(2)));
+      //   renderGraph(nodesRef.current);
+    }
+  }
+
+  return (
+    <div>
+      <canvas height={600} width={600} ref={canvasRef}></canvas>
+      <Actions
+        generate={generate}
+        zoom={zoom}
+        zoomIn={zoomIn}
+        zoomOut={zoomOut}
+      />
+    </div>
+  );
 };
 
 export default Canvas;
